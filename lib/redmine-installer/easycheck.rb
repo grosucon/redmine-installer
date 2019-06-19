4556baa1b2595d5da4b2908d9a08d2fc4376fdc9
Ondřej Moravčík <moravcik.ondrej@gmail.com>
2019-06-19 12:15:24 +0200

    refs #292513 bundler environment

--- lib/redmine-installer/easycheck.rb
+++ lib/redmine-installer/easycheck.rb
@@ -7,14 +7,16 @@ module RedmineInstaller
     EASYCHECK_SH = 'https://raw.githubusercontent.com/easyredmine/easy_server_requirements_check/master/easycheck.sh'
 
     def self.run
-      if Kernel.system('which', 'wget')
-        Open3.pipeline(['wget', EASYCHECK_SH, '-O', '-', '--quiet'], 'bash')
+      Bundler.with_clean_env do
+        if Kernel.system('which', 'wget')
+          Open3.pipeline(['wget', EASYCHECK_SH, '-O', '-', '--quiet'], 'bash')
 
-      elsif Kernel.system('which', 'curl')
-        Open3.pipeline(['curl', EASYCHECK_SH, '--output', '-', '--silent'], 'bash')
+        elsif Kernel.system('which', 'curl')
+          Open3.pipeline(['curl', EASYCHECK_SH, '--output', '-', '--silent'], 'bash')
 
-      else
-        error 'Neither wget nor curl was found'
+        else
+          error 'Neither wget nor curl was found'
+        end
       end
 
       puts
--- lib/redmine-installer/version.rb
+++ lib/redmine-installer/version.rb
@@ -1,3 +1,3 @@
 module RedmineInstaller
-  VERSION = '2.3.1.beta'
+  VERSION = '2.3.2.beta'
 end
